c4b3b2e690b79b064ffd48646b5bd6690fdf62b3
Matheus Lima <matheus.lima@concretesolutions.com.br>
2017-10-09 15:23:00 -0300

    Adding Enzyme and multi-level component tests

---
+++ __tests__/testSetup.js
@@ -0,0 +1,17 @@
+const ReactTestRenderer = require("react-test-renderer")
+const Enzyme = require("enzyme")
+const Adapter = require("enzyme-adapter-react-16")
+
+Enzyme.configure({ adapter: new Adapter() })
+
+global.shallow = Enzyme.shallow
+global.render = Enzyme.render
+global.mount = Enzyme.mount
+
+global.requestAnimationFrame = function (callback) {
+  setTimeout(callback, 0);
+}
+
+console.error = message => {
+  throw new Error(message)
+};
