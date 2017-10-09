d455a80e20b40f7f9e51132f73673b3c21f9db72
Matheus Lima <matheusml90@gmail.com>
2017-10-08 21:52:32 -0300

    Refactoring code

--- src/interfaces/Props.ts
+++ src/interfaces/Props.ts
@@ -1,10 +1,5 @@
+import Store from "./Store"
+
 export default interface Props {
   store: Store
-}
-
-export interface Store {
-  setState: Function
-  subscribe: Function
-  unsubscribe: Function
-  getState: Function
 }
