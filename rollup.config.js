2fc5ff56cfb3251f7c228a540f898ec18f979fb4
Matheus Lima <matheus.lima@concretesolutions.com.br>
2017-10-18 17:43:08 -0200

    Fixing rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -3,30 +3,33 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import peerDeps from 'rollup-plugin-peer-deps-external'
 
-import multiEntries from './src/build/multiEntries'
-
 const format = process.env.NODE_ENV
-const file = `dist/redux-zero.${format}.js`
 
-const config = {
-  input: ['./src/index.ts', './src/react/index.ts'],
-  name: 'redux-zero',
-  sourcemap: true,
-  output: {
-    file,
-    format,
-  },
-  plugins: [
-    multiEntries(),
-    peerDeps(),
-    typescript(),
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true,
-    }),
-    commonjs(),
-  ],
+function getConfig(input, file) {
+  return {
+    input,
+    name: 'redux-zero',
+    sourcemap: true,
+    output: {
+      file,
+      format,
+    },
+    plugins: [
+      peerDeps(),
+      typescript(),
+      resolve({
+        jsnext: true,
+        main: true,
+        browser: true,
+      }),
+      commonjs(),
+    ],
+  }
 }
+
+const config = [
+  getConfig('./src/index.ts', `dist/redux-zero.${format}.js`),
+  getConfig('./src/react/index.ts', `dist/react/redux-zero.${format}.js`),
+]
 
 export default config
