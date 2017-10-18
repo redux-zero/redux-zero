83024611a786e9ba621a0941c65d5dac278c61c3
Matheus Lima <matheus.lima@concretesolutions.com.br>
2017-10-18 15:12:41 -0200

    Removing UMD

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,15 @@
 import typescript from 'rollup-plugin-typescript2'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import uglify from 'rollup-plugin-uglify'
-import filesize from 'rollup-plugin-filesize'
 import peerDeps from 'rollup-plugin-peer-deps-external'
 
 import multiEntries from './src/build/multiEntries'
 
 const format = process.env.NODE_ENV
-const isUmd = format === 'umd'
-const file = `dist/redux-zero.${isUmd ? 'min' : format}.js`
+const file = `dist/redux-zero.${format}.js`
 
 const config = {
-  input: ['./src/index.ts', './src/react/index.ts'],
+  input: './src/index.ts',
   name: 'redux-zero',
   sourcemap: true,
   output: {
@@ -31,7 +28,5 @@ const config = {
     commonjs(),
   ],
 }
-
-isUmd && config.plugins.push(uglify(), filesize())
 
 export default config
