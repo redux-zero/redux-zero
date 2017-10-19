32d34a01a65b30bae91cc6462ee8a4c7b34c41bb
Matheus Lima <matheusml90@gmail.com>
2017-10-18 22:57:19 -0200

    Removing index files

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var createStore_1 = require("./store/createStore");
-exports.default = createStore_1.default;
-//# sourceMappingURL=index.js.map
