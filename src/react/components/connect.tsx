af1df6535209da4ae84e410918447ce0ec7b2dae
Dillon Mulroy <dillon.mulroy@gmail.com>
2017-10-21 19:12:49 -0400

    Provide Connect component with render callback as an option alongside connect HOC

--- src/react/components/connect.tsx
+++ src/react/components/connect.tsx
@@ -1,37 +1,40 @@
-import * as React from "react"
+import * as React from 'react';
 
-import shallowEqual from "../../utils/shallowEqual"
-import propValidation from "../../utils/propsValidation"
+import shallowEqual from '../../utils/shallowEqual';
+import propValidation from '../../utils/propsValidation';
+
+export class Connect extends React.Component<any> {
+  static contextTypes = {
+    store: propValidation
+  };
+  unsubscribe;
+  state = this.getProps();
+  componentWillMount() {
+    this.unsubscribe = this.context.store.subscribe(this.update);
+  }
+  componentWillUnmount() {
+    this.unsubscribe(this.update);
+  }
+  getProps() {
+    const { mapToProps } = this.props;
+    const state = (this.context.store && this.context.store.getState()) || {};
+    return mapToProps(state, this.props);
+  }
+  update = () => {
+    const mapped = this.getProps();
+    if (!shallowEqual(mapped, this.state)) {
+      this.setState(mapped);
+    }
+  };
+  render() {
+    return this.props.children(this.state);
+  }
+}
 
 export default function connect(mapToProps) {
-  return Child =>
-    class Connected extends React.Component {
-      static contextTypes = {
-        store: propValidation
-      }
-      unsubscribe
-      state = this.getProps()
-      componentWillMount() {
-        this.unsubscribe = this.context.store.subscribe(this.update)
-      }
-      componentWillUnmount() {
-        this.unsubscribe(this.update)
-      }
-      getProps() {
-        const state =
-          (this.context.store && this.context.store.getState()) || {}
-        return mapToProps(state, this.props)
-      }
-      update = () => {
-        const mapped = this.getProps()
-        if (!shallowEqual(mapped, this.state)) {
-          this.setState(mapped)
-        }
-      }
-      render() {
-        return (
-          <Child store={this.context.store} {...this.props} {...this.state} />
-        )
-      }
-    }
+  return Child => props => (
+    <Connect mapToProps={mapToProps}>
+      {mappedProps => <Child {...mappedProps} {...props} />}
+    </Connect>
+  );
 }
--- src/react/index.ts
+++ src/react/index.ts
@@ -1,4 +1,4 @@
-import connect from "./components/connect"
+import connect, { Connect } from "./components/connect"
 import Provider from "./components/Provider"
 
-export { connect, Provider }
+export { connect, Provider, Connect }
