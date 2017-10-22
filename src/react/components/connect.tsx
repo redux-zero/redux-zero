c4dea0ec48f03253c453dcf54fb3f5c00431781e
Dillon Mulroy <dillon.mulroy@gmail.com>
2017-10-21 20:07:20 -0400

    formatting

--- src/react/components/connect.tsx
+++ src/react/components/connect.tsx
@@ -1,33 +1,33 @@
-import * as React from 'react';
+import * as React from "react"
 
-import shallowEqual from '../../utils/shallowEqual';
-import propValidation from '../../utils/propsValidation';
+import shallowEqual from "../../utils/shallowEqual"
+import propValidation from "../../utils/propsValidation"
 
 export class Connect extends React.Component<any> {
   static contextTypes = {
     store: propValidation
-  };
-  unsubscribe;
-  state = this.getProps();
+  }
+  unsubscribe
+  state = this.getProps()
   componentWillMount() {
-    this.unsubscribe = this.context.store.subscribe(this.update);
+    this.unsubscribe = this.context.store.subscribe(this.update)
   }
   componentWillUnmount() {
-    this.unsubscribe(this.update);
+    this.unsubscribe(this.update)
   }
   getProps() {
-    const { mapToProps } = this.props;
-    const state = (this.context.store && this.context.store.getState()) || {};
-    return mapToProps(state, this.props);
+    const { mapToProps } = this.props
+    const state = (this.context.store && this.context.store.getState()) || {}
+    return mapToProps(state, this.props)
   }
   update = () => {
-    const mapped = this.getProps();
+    const mapped = this.getProps()
     if (!shallowEqual(mapped, this.state)) {
-      this.setState(mapped);
+      this.setState(mapped)
     }
-  };
+  }
   render() {
-    return this.props.children(this.state);
+    return this.props.children(this.state)
   }
 }
 
@@ -36,5 +36,5 @@ export default function connect(mapToProps) {
     <Connect mapToProps={mapToProps}>
       {mappedProps => <Child {...mappedProps} {...props} />}
     </Connect>
-  );
+  )
 }
