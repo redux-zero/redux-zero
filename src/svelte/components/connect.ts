8d7a7f3f42491424f77a7a35b75ec547d1c2b5f9
Kiho <kihoc22@gmail.com>
2017-10-27 21:53:27 -0700

    Remove Object.assign

--- src/svelte/components/connect.ts
+++ src/svelte/components/connect.ts
@@ -2,31 +2,34 @@ import shallowEqual from "../../utils/shallowEqual"
 import bindActions from "../../utils/bindActions"
 
 export function getActions(store, actions) {
-    return bindActions(
-        typeof actions === "function" ? actions(store) : actions,
-        store
-    )
+  return bindActions(
+    typeof actions === "function" ? actions(store) : actions,
+    store
+  )
 }
 
 function getDiff(newData, oldData) {
-    const diff = { }
-    let changed = false
-    for (let key in newData) {        
-        if (!shallowEqual(oldData[key], newData[key])) {
-            changed = true
-            diff[key] = newData[key]
-        }
+  const diff = {}
+  let changed = false
+  for (let key in newData) {
+    if (!shallowEqual(oldData[key], newData[key])) {
+      changed = true
+      diff[key] = newData[key]
     }
-    return { diff, changed }
+  }
+  return { diff, changed }
 }
 
 export function connect(component, store, mapToProps) {
-    update()
-    component.on("destroy", store.subscribe(update))
-    function update() {
-        const { diff, changed } = getDiff(mapToProps(store.getState()), component.get())
-        if (changed) {
-            component.set(diff)
-        }
+  update()
+  component.on("destroy", store.subscribe(update))
+  function update() {
+    const { diff, changed } = getDiff(
+      mapToProps(store.getState()),
+      component.get()
+    )
+    if (changed) {
+      component.set(diff)
     }
+  }
 }
