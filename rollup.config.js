477401f703bbc653eaae8c1d0b22aea5e718b3a3
Kiho <kihoc22@gmail.com>
2017-11-06 20:43:26 -0800

    Copy definition files from dist/svelte on build

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from 'rollup-plugin-commonjs'
 import peerDeps from 'rollup-plugin-peer-deps-external'
 import uglify from 'rollup-plugin-uglify'
 import filesize from 'rollup-plugin-filesize'
+import copy from 'rollup-plugin-copy';
 
 const format = process.env.NODE_ENV
 const isUmd = format === 'umd'
@@ -15,7 +16,8 @@ function getFileName(file) {
   return `${file}.js`
 }
 
-function getConfig(input, file) {
+function getConfig(input, file, sub) {
+  const override = { compilerOptions: { declaration: sub ? false : true } };
   const conf = {
     input,
     name: 'redux-zero',
@@ -26,17 +28,24 @@ function getConfig(input, file) {
     },
     plugins: [
       peerDeps(),
-      typescript(),
+      typescript({ tsconfigOverride: override }),
       resolve({
         jsnext: true,
         main: true,
         browser: true,
       }),
-      commonjs(),
+      commonjs(),      
     ],
-  }
+  } 
 
   isUmd && conf.plugins.push(uglify(), filesize())
+
+  if (!isUmd && sub) {
+    console.log('conf.plugins.push', file)
+    conf.plugins.push(
+      copy(sub)
+    )
+  }
 
   return conf
 }
@@ -44,7 +53,10 @@ function getConfig(input, file) {
 const config = [
   getConfig('./src/index.ts', 'dist/redux-zero'),
   getConfig('./src/react/index.ts', 'react/index'),
-  getConfig('./src/svelte/index.ts', 'svelte/index'),
+  getConfig('./src/svelte/index.ts', 'svelte/index', {
+    "dist/svelte": "svelte",
+    verbose: true
+  }),
 ]
 
 export default config
