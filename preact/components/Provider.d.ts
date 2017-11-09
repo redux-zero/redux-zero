653faba74ca96b14164351e35ffb11003dc0e735
Matheus Lima <matheus.lima@concretesolutions.com.br>
2017-11-09 11:33:46 -0200

    Removing preact folder

--- preact/components/Provider.d.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "preact";
-import Props from "../../interfaces/Props";
-import Store from "../../interfaces/Store";
-export default class Provider extends Component<Props, {}> {
-    getChildContext(): {
-        store: Store;
-    };
-    render(): JSX.Element;
-}
