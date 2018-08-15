73f425b08a7218ff210785f7c00dbd7f9471514e
Emiliano Fernandez <emilianohfernandez@gmail.com>
2018-08-15 10:25:14 -0300

    separe devtools interfaces from main file

--- src/devtools/devtoolsMiddleware.ts
+++ src/devtools/devtoolsMiddleware.ts
@@ -1,45 +1,12 @@
 import Store from "../interfaces/Store";
-
-export interface NextAction {
-  key: string;
-  fn: Function;
-}
-
-export interface Action {
-  type: string;
-  name: string;
-}
-
-export interface ReduxAction {
-  type: string;
-  args: any;
-}
-
-export interface MessageState {
-  actionsById: {
-    [index: string]: {
-      action: Action;
-    };
-  };
-  computedStates: Store;
-}
-
-export interface Message {
-  state: string;
-  payload: {
-    id: number;
-    type: string;
-  };
-  type: string;
-}
-
-export interface DevToolsStore extends Store {
-  send: (type: string | ReduxAction, state: object) => void;
-}
-
-export interface DevTools {
-  instance: DevToolsStore | null;
-}
+import {
+  Action,
+  DevTools,
+  NextAction,
+  Message,
+  MessageState,
+  ReduxAction
+} from "./devtoolsMidleware";
 
 let devTools: DevTools = { instance: null };
 let connect;
