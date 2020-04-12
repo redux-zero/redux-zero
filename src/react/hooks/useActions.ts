c13fb05270997ead99dc6654d1ed94af44d48c83
Matheus Lima <matheusml90@gmail.com>
2020-04-12 16:58:37 -0300

    Adding typings to useActions

--- src/react/hooks/useActions.ts
+++ src/react/hooks/useActions.ts
@@ -1,14 +1,24 @@
 import { useAction } from "./useAction";
 import { useStore } from "./useStore";
 
-export function useActions(actions) {
+import { Action } from "../../types";
+import Store from "../../interfaces/Store";
+
+type ActionMap<S> = {
+  [key: string]: Action<S>;
+};
+
+type ActionBindFunction<S> = (store: Store<S>) => ActionMap<S>;
+
+export function useActions<S>(actions: ActionBindFunction<S>): ActionMap<S> {
   const store = useStore();
-  const obj = actions(store);
-  let bound = {};
-  Object.keys(obj).forEach(key => {
-    let act = obj[key];
-    let fn = useAction(act);
-    bound[key] = fn;
+  const connectedActions = actions(store);
+
+  const bound = {};
+  Object.keys(connectedActions).forEach(key => {
+    let action = connectedActions[key];
+    let actionHook = useAction(action);
+    bound[key] = actionHook;
   });
   return bound;
 }
