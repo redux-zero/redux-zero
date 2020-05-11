3f4208c081154c6461a51f52ca419a6b273a7969
Chad Engler <englercj@live.com>
2020-05-11 10:54:59 -0700

    Better return type for bind actions

--- src/utils/bindActions.ts
+++ src/utils/bindActions.ts
@@ -2,11 +2,13 @@ import bindAction from "./bindAction";
 import Store from "../interfaces/Store";
 import { Action } from "../types";
 
+type OmitFirstArg<F, R> = F extends (x: any, ...args: infer P) => any ? (...args: P) => R : never;
+
 export default function bindActions<S, T extends { [key: string]: Action<S> }>(
   actions: ((store: Store<S>, ownProps) => T) | T,
   store: Store<S>,
   ownProps?: object
-): { [K in keyof T]: (...args: any[]) => Promise<void> | void } {
+): { [K in keyof T]: OmitFirstArg<T[K], Promise<void> | void> } {
   actions = typeof actions === "function" ? actions(store, ownProps) : actions;
 
   let bound: { [key: string]: (...args: any[]) => Promise<void> | void } = {};
@@ -16,5 +18,5 @@ export default function bindActions<S, T extends { [key: string]: Action<S> }>(
     bound[name] = bindAction(action, store);
   }
 
-  return bound as { [K in keyof T]: (...args: any[]) => Promise<void> | void };
+  return bound as { [K in keyof T]: OmitFirstArg<T[K], Promise<void> | void> };
 }
